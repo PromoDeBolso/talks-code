566a729f4d0846f1975f3b119a2c1f2e82285935
evertoncastro <evertoncastro.sp@gmail.com>
2019-02-26 23:21:36 -0300

    Fixes

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
+import sys
+
 from business.pincode import Pincode
 from business.valida_pincode import ValidaPincode
 
 if __name__ == '__main__':
-    p = Pincode('torratorra', 'TOR64321EW') 
-    ValidaPincode().valida(p)
-    
+    args = sys.argv
+    p = Pincode(args[1], args[2]) 
+    is_valid = ValidaPincode().valida(p)
+    print(f'Pincode is valid: {is_valid}.')
